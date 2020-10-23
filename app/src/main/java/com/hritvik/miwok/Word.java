fb8b9e6064c7f6834475dd4b38c942cd576737b7
Hritvik3 <hritvikpatwa@gmail.com>
2020-10-23 21:26:31 +0530

    added real images

--- app/src/main/java/com/hritvik/miwok/Word.java
+++ app/src/main/java/com/hritvik/miwok/Word.java
@@ -3,10 +3,23 @@ package com.hritvik.miwok;
 public class Word {
     // m represents member variables of the class
 private String mDefaultTranslation, mMiwokTranslation;
+private int mImageResourceId=NO_IMAGE_PROVIDED;
 
+// We create a constant so if there is no image then by default the resource id will be this constant
+private static final int NO_IMAGE_PROVIDED = -1 ;
+
+
+    // constructor without image for the phrases activity and even if the image is not there then this will work
     public Word(String DefaultTranslation, String MiwokTranslation) {
         this.mDefaultTranslation = DefaultTranslation;
         this.mMiwokTranslation = MiwokTranslation;
+    }
+
+    // second constructor to make add the corresponding images to the list
+    public Word(String DefaultTranslation, String MiwokTranslation, int ImageResourceId) {
+        this.mDefaultTranslation = DefaultTranslation;
+        this.mMiwokTranslation = MiwokTranslation;
+        this.mImageResourceId = ImageResourceId;
     }
 
     //to get the default translation of the word
@@ -17,5 +30,14 @@ private String mDefaultTranslation, mMiwokTranslation;
     // to get the miwok translation of the word
     public String getMiwokTranslation() {
         return mMiwokTranslation;
+    }
+
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    // will return true if the view has image
+    public boolean hasImage(){
+        return mImageResourceId != NO_IMAGE_PROVIDED;
     }
 }
--- app/src/main/java/com/hritvik/miwok/WordAdapter.java
+++ app/src/main/java/com/hritvik/miwok/WordAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -34,6 +35,14 @@ public class WordAdapter extends ArrayAdapter<Word> {
         TextView secondaryText = listItemView.findViewById(R.id.secondaryText);
         secondaryText.setText(currentWord.getMiwokTranslation());
 
+        ImageView iconView = listItemView.findViewById(R.id.image);
+
+        if (currentWord.hasImage()) {
+            iconView.setImageResource(currentWord.getImageResourceId());
+        }
+        else {
+            iconView.setVisibility(View.GONE);
+        }
 
 
 
